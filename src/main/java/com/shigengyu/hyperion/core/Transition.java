2b0bc7f5ed9f746c3ac62de0fe70d01dc5738fd5
shigengyu <univer.shi@gmail.com>
2014-04-20 23:13:28 +0100

    Update build file

--- src/main/java/com/shigengyu/hyperion/core/Transition.java
+++ src/main/java/com/shigengyu/hyperion/core/Transition.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2013-2014 Gengyu Shi
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,14 +32,40 @@ public @interface Transition {
 
 	Class<? extends WorkflowState>[] fromStates() default {};
 
+	/**
+	 * Whether the transition is hidden. Hidden transitions can be executed programmatically but is not exposed to the
+	 * Hyperion runtime interface
+	 *
+	 * @return
+	 */
 	boolean hidden() default false;
 
+	/**
+	 * The maximum entries if the transition is multi-entry
+	 *
+	 * @return
+	 */
 	int maxEntry() default 1;
 
+	/**
+	 * Whether multi-entry is allowed on this transition, i.e. transition triggering itself
+	 *
+	 * @return
+	 */
 	boolean multiEntry() default false;
 
+	/**
+	 * Name of the transition
+	 *
+	 * @return
+	 */
 	String name() default "";
 
+	/**
+	 * Whether or not to override the values of the {@link TransitionShared} annotation
+	 *
+	 * @return
+	 */
 	boolean override() default true;
 
 	StateTransitionStyle stateTransitionStyle() default StateTransitionStyle.INCREMENTAL;
--- src/main/java/com/shigengyu/hyperion/core/TransitionDefinitionException.java
+++ src/main/java/com/shigengyu/hyperion/core/TransitionDefinitionException.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2013-2014 Gengyu Shi
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,26 +23,21 @@ public class TransitionDefinitionException extends HyperionException {
 
 	public TransitionDefinitionException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public TransitionDefinitionException(String message, Object... args) {
 		super(message, args);
-		// TODO Auto-generated constructor stub
 	}
 
 	public TransitionDefinitionException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public TransitionDefinitionException(String message, Throwable cause, Object... args) {
 		super(message, cause, args);
-		// TODO Auto-generated constructor stub
 	}
 
 	public TransitionDefinitionException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 }
